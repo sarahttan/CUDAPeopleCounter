7f3fc30df80109d74aff123553179dd2a5242b1e
sarahttan <sarah.t.tan@gmail.com>
2015-04-13 17:32:09 -0400

    Added funcs for boxes - untested

--- peopleCounter.c
+++ peopleCounter.c
@@ -18,6 +18,9 @@
 // PRIORITY COMPLETION ORDER
 //  (1) FrameToJPG
 //  (2) FrameSubtraction (testing in progress)
+//  (2) Test create boxes
+//  (3) Test delete boxes
+//  (4) Test print boxes
 //  (3) BlobDetection
 //  (4) MergeBlobs
 //  (5) BlobDirection  (testing in progress)
@@ -118,14 +121,48 @@ int frameSubtraction(frame_t *frame, frame_t *frame2, frame_t *res){
 }
 
 
-int createNewBox(frame_t *frame) {
-    //TODO: create a new bounding box in the frame
+//TODO: Testing in progress
+// Create a new box with centroid (c_x, c_y), height and width
+int createNewBox(frame_t *frame, int c_x, int c_y, int height, int width) {
+    //create a new bounding box in the frame
+    
+    // initialize values
+    box_t *newB = (box_t *)malloc(sizeof(box_t));
+    newB->centroid_x = c_x;
+    newB->centroid_y = c_y;
+    newB->height = height;
+    newB->width = width;
+
+    // Update tag
+    if (frame->boxes == NULL){
+        newB->tag = 0;
+    } else {
+        newB->tag = frame->boxes->tag + 1;
+    }
+
+    // Update linked list
+    newB->next = frame->boxes;
+    frame->boxes = newB;
     return 0;
 }
 
+//TODO: Testing in progress
 int deleteBox(frame_t *frame, box_t *b){
-    //TODO: remove bounding box from frame
-    return 0;
+    box_t *tmp = frame->boxes;
+    if (tmp == NULL) {
+        printf("deleteBox: No boxes found\n");
+        return 1;
+    }
+    while (tmp != NULL) {
+        if (tmp->next == b) {
+            tmp->next = b->next;
+            free(b);
+            return 0;
+        }
+        tmp = tmp->next;
+    }
+    printf("deleteBox: Unable to find box to delete\n");
+    return 1;
 }
 
 int blurImage(frame_t *frame) {
@@ -169,13 +206,14 @@ int mergeBlobs(frame_t *frame){
     return 0;
 }
 
+//TODO: Testing in progress
 int findBlobDirection(frame_t *frame, frame_t *frame2, frame_t *res){
     //based on the distance and similarities between blobs in the two images, associate the closest blob to each other and then update the direction
-    if ((frame == NULL) && (frame2 == NULL) && (res == NULL)){
+    if ((frame == NULL) || (frame2 == NULL) || (res == NULL)){
         printf("findBlobDirection: Can't find Blob dir - frame is not initialized\n");
     }
 
-    if ((frame->boxes == NULL) && (frame2->boxes == NULL) && (res->boxes == NULL)){
+    if ((frame->boxes == NULL) || (frame2->boxes == NULL) || (res->boxes == NULL)){
         printf("findBlobDirection: Can't find blob dir - frame->boxes not initialized\n");
     }
 
@@ -188,13 +226,113 @@ int findBlobDirection(frame_t *frame, frame_t *frame2, frame_t *res){
     return 0;
 }
 
+//TODO: Testing in progress
 box_t *getBoundingBoxes(frame_t *frame) {
     //based on the image, get the bounding boxes and return them
     if (frame == NULL){
         printf("getBoundingBoxes: Can't get bounding box, frame is NULL\n");
         return NULL;
     }
+
     return frame->boxes;
+}
+
+//TODO: Testing in progress
+int drawBoxOnImage(frame_t *frame, frame_t *res) {
+    // draws a white box for every bounding box given the values of frame
+    if ((frame == NULL) || (res == NULL)) {
+        printf("drawBoxOnImage: Can't draw box, frame is NULL\n");
+    }
+    
+    // initialize
+    box_t *head = frame->boxes;
+    box_t *tmp = head;
+    int width;
+    int height;
+    int cx;
+    int cy;
+    char done = 0;
+
+    // Check if there are boxes available
+    if (head == NULL){
+        LOG_ERR("drawBoxOnImage: No boxes found in frame\n");
+        return 1;
+    }
+    while (tmp != NULL){
+        //get width, height, and centroid of each box
+        width = tmp->width;
+        height = tmp->height;
+        cx = tmp->centroid_x;
+        cy = tmp->centroid_y;
+    
+        //draw box onto the result frame
+        int i,j,cr1,cr2;
+        cr1 = cx-width/2;
+        cr2 = cx+width/2;
+        for (j=cy-height/2; j < cy+height/2; j++){
+        // draw in the left and right cols in the result image
+            //Draw left column
+            if ((cr1 < 0) || (cr1 >= frame->image->width)){
+                // column 1 is out of bounds
+                LOG_ERR("drawBoxOnImage: left column %d is out of bounds\n", cr1);
+                done++;
+            } else {
+                // draw in the values
+                res->image->data[j*width+cr1].L = 1;
+                res->image->data[j*width+cr1].A = 0;
+                res->image->data[j*width+cr1].B = 0;
+            }
+            
+            // Draw right column
+            if ((cr2 < 0) || (cr2 >= frame->image->width)) {
+                LOG_ERR("drawBoxOnImage: right column %d is out of bounds\n",cr2);
+                done++;
+            } else {
+                // draw in the values
+                res->image->data[j*width+cr2].L = 1;
+                res->image->data[j*width+cr2].A = 0;
+                res->image->data[j*width+cr2].B = 0;   
+            }
+            if (done >= 2) {
+                break;
+            }
+        }
+
+
+        cr1 = cy-height/2;
+        cr2 = cy+height/2;
+        for (i=cx-width/2; i < cx+width/2; j++){
+        // draw in the top and bottom rows in the result image
+            //Draw top row
+            if ((cr1 < 0) || (cr1 >= frame->image->height)){
+                // column 1 is out of bounds
+                LOG_ERR("drawBoxOnImage: top row %d is out of bounds\n", cr1);
+                done++;
+            } else {
+                // draw in the values
+                res->image->data[i*width+cr1].L = 1;
+                res->image->data[i*width+cr1].A = 0;
+                res->image->data[i*width+cr1].B = 0;
+            }
+
+            // Draw right column
+            if ((cr2 < 0) || (cr2 >= frame->image->height)) {
+                LOG_ERR("drawBoxOnImage: bottom row %d is out of bounds\n",cr2);
+                done++;
+            } else {
+                // draw in the values
+                res->image->data[i*width+cr2].L = 1;
+                res->image->data[i*width+cr2].A = 0;
+                res->image->data[i*width+cr2].B = 0;
+            }
+            if (done >= 2) {
+                break;
+            }
+        }
+
+        tmp = tmp->next;
+    }
+    return 0;
 }
 
 int frameToJPG(frame_t *frame){
