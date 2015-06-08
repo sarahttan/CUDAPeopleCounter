24aad3c32320753e90b5d75fb324deb95cffa1ae
sarahttan <sarah.t.tan@gmail.com>
2015-06-08 16:51:50 -0400

    Cleaned up Webpage Directory

--- gCUDA.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git add tests/fullTestCUDA.cu
-git add peopleCounterCUDA.cu
-git add peopleCounter2.h
-git add readjpeg.c
-git add Makefile
--- tests/.#fullTest.c
+++
@@ -1,1 +0,0 @@
-sttan@ghc43.ghc.andrew.cmu.edu.30103
