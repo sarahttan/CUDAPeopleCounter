eff1ddcf7e094ae589ca673ed3474ea09dca1306
sarahttan <sarah.t.tan@gmail.com>
2015-06-08 16:52:21 -0400

    Revert "Revert "Cleaned up Webpage Directory""
    This reverts commit e7aede18b334c35400dde44335974e9f565d8d1f.

--- doTest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./fullTest frames/frame01.jpg frames/frame04.jpg > result01-04.txt
-./fullTest frames/frame04.jpg frames/frame07.jpg > result04-07.txt
-./fullTest frames/frame07.jpg frames/frame10.jpg > result07-10.txt
-./fullTest frames/frame10.jpg frames/frame13.jpg > result10-13.txt
-./fullTest frames/frame13.jpg frames/frame16.jpg > result13-16.txt
